af286df298179c2b735d9f77467c93b1bfb76836
pythonicforge <pythonicforge@gmail.com>
2025-02-04 18:25:25 +0530

    added: POST and CSRF token

--- text-utils/text-utils/views.py
+++ text-utils/text-utils/views.py
@@ -5,33 +5,34 @@ def home(request):
     return render(request, 'index.html', citations)
 
 def analyse_text(request):
-    text = request.GET.get('text', '')
-    operations = request.GET.getlist('operation', [])
-    analyzed = text
+    if request.method == 'POST':
+        text = request.POST.get('text', '')
+        operations = request.POST.getlist('operation', [])
+        analyzed = text
 
-    if 'removepunc' in operations:
-        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-        analyzed = ''.join(char for char in analyzed if char not in punctuations)
+        if 'removepunc' in operations:
+            punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+            analyzed = ''.join(char for char in analyzed if char not in punctuations)
 
-    if 'capitalize' in operations:
-        analyzed = analyzed.upper()
+        if 'capitalize' in operations:
+            analyzed = analyzed.upper()
 
-    if 'newlineremover' in operations:
-        analyzed = analyzed.replace('\n', '').replace('\r', '')
+        if 'newlineremover' in operations:
+            analyzed = analyzed.replace('\n', '').replace('\r', '')
 
-    if 'spaceremover' in operations:
-        analyzed = ' '.join(analyzed.split())
+        if 'spaceremover' in operations:
+            analyzed = ' '.join(analyzed.split())
 
-    purposes = []
-    if 'removepunc' in operations:
-        purposes.append("Removed Punctuations")
-    if 'capitalize' in operations:
-        purposes.append("Capitalized Text")
-    if 'newlineremover' in operations:
-        purposes.append("Removed New Lines")
-    if 'spaceremover' in operations:
-        purposes.append("Removed Extra Spaces")
+        purposes = []
+        if 'removepunc' in operations:
+            purposes.append("Removed Punctuations")
+        if 'capitalize' in operations:
+            purposes.append("Capitalized Text")
+        if 'newlineremover' in operations:
+            purposes.append("Removed New Lines")
+        if 'spaceremover' in operations:
+            purposes.append("Removed Extra Spaces")
 
-    purpose = " and ".join(purposes) if purposes else "No Operation Performed"
-    
-    return render(request, 'analyse_text.html', {'analyzedText': analyzed, 'purpose': purpose})
+        purpose = " and ".join(purposes) if purposes else "No Operation Performed"
+        
+        return render(request, 'analyse_text.html', {'analyzedText': analyzed, 'purpose': purpose})
