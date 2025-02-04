7a072a4b339c84372c87db0429b95384bcbe98a2
pythonicforge <pythonicforge@gmail.com>
2025-02-04 18:03:58 +0530

    added: README

---
+++ text-utils/README.md
@@ -0,0 +1,35 @@
+# Text-Utils  
+
+**Text-Utils** is a simple Django web application for performing text operations such as removing punctuation, capitalizing text, and cleaning up spaces.  
+
+## Features  
+- Remove punctuation  
+- Capitalize text  
+- Remove newlines  
+- Eliminate extra spaces  
+
+## Getting Started  
+
+1. Clone the repository:  
+   ```bash
+   git clone <https://github.com/pythonicforge/learning-django.git>
+   cd learning-django/text-utils
+   ```
+
+2. Set up a virtual environment and install dependencies:  
+   ```bash
+   python3 -m venv env
+   source env/bin/activate  
+   pip install -r requirements.txt
+   ```
+
+3. Run the server:  
+   ```bash
+   python manage.py runserver
+   ```
+
+Visit [http://127.0.0.1:8000](http://127.0.0.1:8000) in your browser.  
+
+---
+
+Crafted with ❤️ by [Hardik Jaiswal](https://github.com/pythonicforge)  
---
+++ text-utils/text-utils/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+def home(request):
+    citations = {'author': 'Hardik Jaiswal'}
+    return render(request, 'index.html', citations)
+
+def analyse_text(request):
+    text = request.GET.get('text', '')
+    operations = request.GET.getlist('operation', [])
+    analyzed = text
+
+    if 'removepunc' in operations:
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ''.join(char for char in analyzed if char not in punctuations)
+
+    if 'capitalize' in operations:
+        analyzed = analyzed.upper()
+
+    if 'newlineremover' in operations:
+        analyzed = analyzed.replace('\n', '').replace('\r', '')
+
+    if 'spaceremover' in operations:
+        analyzed = ' '.join(analyzed.split())
+
+    purposes = []
+    if 'removepunc' in operations:
+        purposes.append("Removed Punctuations")
+    if 'capitalize' in operations:
+        purposes.append("Capitalized Text")
+    if 'newlineremover' in operations:
+        purposes.append("Removed New Lines")
+    if 'spaceremover' in operations:
+        purposes.append("Removed Extra Spaces")
+
+    purpose = " and ".join(purposes) if purposes else "No Operation Performed"
+    
+    return render(request, 'analyse_text.html', {'analyzedText': analyzed, 'purpose': purpose})
